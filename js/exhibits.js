70df2bc18c2a44f963a5011bc3cdfb3b9973ee92
Travis Taylor <travisdoesmath@gmail.com>
2020-04-19 11:21:14 -0500

    added titles and descriptions

--- js/exhibits.js
+++ js/exhibits.js
@@ -1,80 +1,87 @@
 exhibits = [
     {
         id: 'covid',
+        title: "COVID-19 Deaths",
         link: "http://travisdoesmath.github.io/covid-viz/",
+        description: "Tracking deaths in the US due to COVID<span>&#8209;19</span> versus top causes of death identified by the CDC",
         src: "media/covid.png",
-        alt: "COVID-19 Deaths",
         type: "img"
     },
     {
         id: 'pendulum',
         link: "https://bl.ocks.org/travisdoesmath/32eea7d1cb47c18bfabcab65abc9b36c",
+        title: 'Double Pendulums',
+        description: "Showing the sensitive dependence on initial conditions for double pendulums, a simple chaotic system",
         type: "d3"
     },
     {
         id: 'snake',
         link: "http://travisdoesmath.github.io/d3-snake/",
+        title: 'D3 Snake',
+        description: 'The classic snake game, implemented with D3.js',
         type: "d3"
     },
     {
         id: 'woz',
         link: "#",
         src: "http://travisrtaylor.com/media/woz.webm#t=1.75",
-        alt: "Time-rotated Wizard of Oz",
+        title: "Time-rotated Wizard of Oz",
+        description: "A small slice of the Wizard of Oz from a different perspective. We were never in Kansas. We were always in Kansas.",
         type: "video/webm"
     },
     {
         id: 'heart',
         link: "https://shirt.woot.com/offers/i-heart-math",
         src: "media/i-heart-math.png",
-        alt: "I Heart Math",
+        title: "I Heart Math",
+        description: "This one time I designed a shirt for woot.com. I'm kinda fond of math.",
         type: "img"
     },
     {
         id: 'photo',
         link: "http://www.travistaylorphotography.com/",
         src: "media/travistaylorphoto.png",
-        alt: "Travis Taylor Photo",
+        title: "Travis Taylor Photo",
+        description: "My photography portfolio. Mostly travel and portrait work.",
         type: "img"
     },
     {
         id: 'comfy-weather',
         link: "http://travisdoesmath.github.io/comfy-weather",
         src: "media/comfy-weather.png",
-        alt: "Comfy Weather",
+        title: "Comfy Weather",
+        description: "After a particularly sudden cold-snap in Chicago, I started researching new cities to live in. I stuck with Chicago.",
         type: "img"
     },
     {
         id: 'life',
         link: "http://travisdoesmath.github.io/d3-life/",
+        title: 'D3 Life',
+        description: "Conway's Game of Life, implemented in D3.js",
         type: 'd3'
     },
-    // {
-    //     id: 'life-old',
-    //     link: "http://travisdoesmath.github.io/d3-life/",
-    //     src: "media/d3life.png",
-    //     alt: "Conway's Game of Life in D3",
-    //     type: "img"
-    // },
     {
         id: 'mudge',
         link: "https://open.spotify.com/artist/2gr6qq2679SIflhvIq9JHw",
         src: "media/mudge.jpg",
-        alt: "Mudge",
+        title: "Mudge",
+        description: "I made an album of middling quality electronic songs. It ended up on Spotify.",
         type: "img"
     },
     {
         id: 'standard-deviations',
         link: "#",
         src: "media/stddev.jpg",
-        alt: "Standard Deviations",
+        title: "Standard Deviations",
+        description: "Taking collections of photographs I've taken and calculating the standard deviation across each pixel",
         type: "img"
     },
     {
         id: 'golden',
         link: "https://bl.ocks.org/travisdoesmath/f7dc3cc59316d3fad39b347b805b2730",
         src: "media/golden-curl.png",
-        alt: "Golden Curl",
+        title: "Golden Curl",
+        description: "Playing with golden spirals as limits of matrix interpolations, and then making it curl and uncurl",
         type: "img"
     },
 
